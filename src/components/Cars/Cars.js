272ab93b6d5be4c849c6c2c0e1d628ec19e16619
DimanJacky <maslov-mda@yandex.ru>
2020-09-01 23:28:19 +0300

    commit

--- src/components/Cars/Cars.js
+++ src/components/Cars/Cars.js
@@ -20,31 +20,15 @@ export default class Cars extends Component {
             const response = await axios.post('http://ajax/projectcars/');
 
             this.setState({cars: response.data})
-
-            console.log(response.data)
         } catch (e) {
             console.log(e)
         }
     }
 
     render() {
-        // console.log("render")
-        // console.log(this.state)
-
-        // const cars = this.state.cars
-
-        //console.log(Array.isArray(cars))
 
         return (
             <div>
-                    {/*{*/}
-                    {/*    (cars.length != 0) ?*/}
-                    {/*    cars.map((car, i) => {*/}
-                    {/*        const {name, year, color, namedriver, id} = car*/}
-                    {/*        return <Car key={i} {...car} />*/}
-                    {/*    }) : null*/}
-                    {/*}*/}
-
                 {this.renderCars()}
             </div>
         )
